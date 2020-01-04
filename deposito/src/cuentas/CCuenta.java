0c0547fc70801d7134c4ad34a5450910385e870b
Propietario <Propietario@1487340573>
2020-01-04 19:32:43 +0100

    Ejercicio Javadoc

--- deposito/src/cuentas/CCuenta.java
+++ deposito/src/cuentas/CCuenta.java
@@ -1,5 +1,12 @@
 package cuentas;
 
+/**
+ * esta clase describe el comportamiento básico de una cuenta bancaria.
+ * 
+ * @author Raquel
+ * @since 04/01/2020
+ */
+
 public class CCuenta {
 
 	private String nombre;
@@ -20,19 +27,35 @@ public class CCuenta {
 		return getSaldo();
 	}
 
+	/**
+	 * 
+	 * @param cantidad
+	 * @throws Exception
+	 */
 	public void ingresar(double cantidad) throws Exception {
 		if (cantidad < 0)
 			throw new Exception("No se puede ingresar una cantidad negativa");
 		setSaldo(getSaldo() + cantidad);
 	}
 
+	/**
+	 * 
+	 * @param cantidad
+	 * @throws Exception
+	 */
+
 	public void retirar(double cantidad) throws Exception {
 		if (cantidad <= 0)
 			throw new Exception("No se puede retirar una cantidad negativa");
 		if (estado() < cantidad)
-			throw new Exception("No se hay suficiente saldo");
+			throw new Exception("No hay suficiente saldo");
 		setSaldo(getSaldo() - cantidad);
 	}
+
+	/**
+	 * 
+	 * @return devuelve el resultado de la operación
+	 */
 
 	private String getNombre() {
 		return nombre;
